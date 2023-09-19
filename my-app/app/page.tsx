a2dec99764413c7edd601a3c4ec632a7598d9034
ThiagoCarvalh0 <carvalho159263@gmail.com>
2023-09-19 12:34:20 -0300

    init project

---
+++ my-app/app/page.tsx
@@ -0,0 +1,24 @@
+import { fetchWrapper } from '@/services/fetchService';
+import Image from 'next/image';
+
+type PostProps = {
+  id: number;
+  title: string;
+};
+
+export default async function Home() {
+  const response = await fetchWrapper<{ data: PostProps[] }>('/examples', {
+    method: 'GET',
+  });
+  response ? console.log(response.data) : null;
+  return (
+    <div>
+      {response.data.map((item) => (
+        <div key={item.id}>
+          <h1>{item.title}</h1>
+          <p>{item.id}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
